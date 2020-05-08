471e16cdc81b5026c4129c8cb9b83d4474bfd760
fulviodenza <fulviodenza823@gmail.com>
2020-05-08 14:47:44 +0200

    repair test

---
+++ src/z80.c
@@ -0,0 +1,241 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "z80.h"
+#include "instructionSet.h"
+
+z80_info_s status;
+
+int fetch(z80_info_s *status, op_code opcode) 
+{
+
+    uint16_t temp;
+    uint16_t shifted;
+
+    switch(memory[status->m_pc++]) { //switch memory[m_pc]
+
+        case nop:
+        /**
+         * nop does absolutely nothing
+        */
+        status->elapsed_cycles+=4;
+        break;
+
+        case ld_bc_xx:
+        /**
+         * ld_bc_xx stores xx 
+         * (16 bit register) into BC
+        */
+        temp = memory[status->m_pc++];
+        temp<<=8;
+        temp = temp | memory[status->m_pc++];
+        BC = temp;
+        status->elapsed_cycles+=10;
+        break;
+        
+        case ld_bc_a:
+        /**
+         * ld_bc_a stores a into the memory 
+         * location pointed to by BC
+        */
+        *(&BC) = A;
+        status->elapsed_cycles += 7;
+        break;
+
+        case inc_bc:
+        /**
+         * Adds one to BC
+        */
+        BC++;
+        status->elapsed_cycles += 6;
+        break;
+
+        case inc_b:
+        /**
+         * Adds one to B
+        */
+        if(B > SCHAR_MAX)
+        {
+            status->flags.pv = 1;
+        }
+        B++;        
+        status->elapsed_cycles += 4;
+        break;
+
+        case dec_b:
+        /**
+         * Subtract one to B
+        */
+        if(B < SCHAR_MIN)
+        {
+            status->flags.pv = 1;
+        }
+        B--;
+        status->elapsed_cycles += 4;
+        break;
+
+        case ld_b_x:
+        /**
+         * Loads x into b.
+        */
+        temp = memory[status->m_pc++];
+        B = temp;
+        status->elapsed_cycles += 7;
+        break;
+
+        case rlca:
+        /**
+         * the contents of A are rotated left one bit position.
+         * The contents of bit 7 are copied to the carry flag and
+         * bit 0.
+        */
+        shifted = A << 1;
+        //Flags affections
+        status->flags.c = A >> 7;
+        status->flags.h = 0;
+        status->flags.n = 0;
+        //rotation
+        A = shifted | status->flags.c;
+        status->elapsed_cycles += 4;
+        break;
+
+        case ex_af_af1:
+        /**
+         * Exchanges the 16-bit
+         * contents of af and af'
+        */
+        temp = AF;
+        AF = AF1;
+        AF1 = temp;
+        status->elapsed_cycles += 4;
+        break;
+        
+        case add_hl_bc:
+        /**
+         * The value of BC is added to HL
+        */
+        if(HL > 0 && BC >0 || HL < 0 && BC < 0){
+            status->flags.c = 1;
+        }
+        if(HL > 0 && BC < 0 || HL < 0 && BC > 0){
+            status->flags.n = 1;
+        }
+        //HALF CARRY TO IMPLEMENT
+        HL += BC;
+        status->elapsed_cycles += 11;
+        break;
+
+        case ld_a_bc:
+        /**
+         * The contents of the memory
+         * location specified by the 
+         * contents of the BC register
+         * pair are loaded to the
+         * Accumulator
+        */
+        A = *(&BC);
+        status->elapsed_cycles += 7;
+        break;
+
+        case dec_bc:
+        /**
+         * Substracts one from BC
+        */
+        BC--;
+        status->elapsed_cycles += 6;
+        break;
+
+        case inc_c:
+        /**
+         * Adds one to C
+        */
+        C++;
+        status->elapsed_cycles += 4;
+        break;
+
+        case dec_c:
+        /**
+         * Subtracts one to C
+        */
+        C--;
+        status->elapsed_cycles += 4;
+        break;
+
+        case ld_c_x:
+        /**
+         * Loads x to C
+        */
+        C = memory[status->m_pc++];
+        status->elapsed_cycles += 7;
+        break;
+
+        case rrca:
+        /**
+         * The contents of A are rotated right one
+         * bit position. The contents of bit 0 are copied
+         * to the carry flag and bit 7
+        */
+        shifted = A >> 1;
+        //Flags affections
+        status->flags.c = A << 7;
+        status->flags.h = 0;
+        status->flags.n = 0;
+        //rotation
+        A = shifted | status->flags.c;
+        status->elapsed_cycles += 4;
+        break;
+        
+    }
+}
+
+int emulation (int len) 
+{
+    status.m_pc = 0;
+    status.elapsed_cycles = 0;
+    status.m_sp = 32767;
+    
+    while(len > 0){
+        fetch(&status, memory[status.m_pc]);
+        len--;
+    }
+}
+
+int main(int argc, char *argv[]){
+
+    /*
+    memory[0] = 0x0E;
+    memory[1] = 0xFF;
+    memory[2] = 0xFE;
+    C = 5;
+    printf("%d\n",C);
+    emulation(memory[0]);
+    printf("%d\n",C);
+    */
+
+    if(argc == 1) {
+
+        fprintf(stderr, "usage: ./a.out file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *file_name = argv[1];
+    FILE *fd; 
+    long filelen;
+
+    fd = fopen(file_name, "rb"); //read mode
+    fseek(fd, 0, SEEK_END);
+    filelen = ftell(fd);
+    rewind(fd);
+
+    fread(memory, 1, filelen, fd);
+    fclose(fd);
+
+    for(int i = 0; i < filelen; i++){
+
+        printf("memory: %d\n", memory[i]);
+    }
+
+    emulation(filelen);
+}
---
+++ src/z80.h
@@ -0,0 +1,73 @@
+#pragma once
+/*
+    This is main header of the z80 emulator. This file contains
+    most of data structures used in this project
+*/
+
+/*
+    The instruction cycle is composed by an Opcode Fetch,
+    a Memory read and a Memory Write.
+    At the beginning of M1 machine cycle the PC is placed on the address bus
+*/
+
+typedef union
+{
+    uint8_t r1;
+    uint8_t r2;
+    uint16_t rr;
+} z80_register;
+
+typedef struct z80_info_s 
+{
+    z80_register m_regs[8];
+    uint16_t m_pc;
+    uint16_t m_sp;
+    uint8_t m_index[2];
+    int8_t elapsed_cycles;
+
+    struct {
+        int c:1;
+        int n:1;
+        int pv:1;
+        int h:1;
+        int z:1;
+        int s:1;
+        int unused1:1;
+        int unused2:1;
+    } flags;
+    
+} z80_info_s;
+
+z80_info_s z80;
+
+#define A z80.m_regs[0].r1
+#define F z80.m_regs[0].r2
+#define AF z80.m_regs[0].rr
+#define B z80.m_regs[1].r1
+#define C z80.m_regs[1].r2
+#define BC z80.m_regs[1].rr
+#define D z80.m_regs[2].r1
+#define E z80.m_regs[2].r2
+#define DE z80.m_regs[2].rr
+#define H z80.m_regs[3].r1
+#define L z80.m_regs[3].r2
+#define HL z80.m_regs[3].rr
+#define A1 z80.m_regs[4].r1
+#define F1 z80.m_regs[4].r2
+#define AF1 z80.m_regs[4].rr
+#define B1 z80.m_regs[5].r1
+#define C1 z80.m_regs[5].r2
+#define BC1 z80.m_regs[5].rr
+#define D1 z80.m_regs[6].r1
+#define E1 z80.m_regs[6].r2
+#define DE1 z80.m_regs[6].rr
+#define H1 z80.m_regs[7].r1
+#define L1 z80.m_regs[7].r2
+#define HL1 z80.m_regs[7].rr
+
+#define PC z80.m_pc
+#define SP z80.m_sp
+#define IX z80.m_index[0]
+#define IY z80.m_index[1]
+
+uint8_t memory[32768];
