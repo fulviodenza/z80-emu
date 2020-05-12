ed2c94b66acfb5f7f28442a60f9d82cc0c643ce4
fulviodenza <fulviodenza@hotmail.it>
2020-05-13 00:18:35 +0200

    z80 asm compiler in C (cont'd)

--- src/compile.c
+++ src/compile.c
@@ -6,20 +6,39 @@
 #include "instructionSet.h"
 #include "z80.h"
 
-int main()
-{
-    const char filename[] = "highscore.dat";
-    int score = 1088;
-    FILE *hs;
+#define MAXCHAR 1000
 
+int main(int argc, char *argv[]) {
+
+    if(argc != 2) {
+        fprintf(stderr,"Open just one file!\n");
+        return 1;
+    }
+
+    char *str[MAXCHAR];
+    char *filename = argv[1];
+
+    FILE *input_file = fopen(filename, "r");
+
+    if(input_file == NULL) {
+        printf("Could not open file %s",filename);
+        return 1;
+    }
+
+    FILE *output_file = fopen("out.z80", "wb");
+    
     /* create the file and write the value */
-    printf("Writing high score: %d\n",score);
-    hs = fopen(filename,"w");
-    if( hs == NULL)
+    if( output_file == NULL)
     {
-        fprintf(stderr,"Error writing to %s\n",filename);
+        fprintf(stderr,"Error writing to %s\n",output_file);
         return(1);
     }
-    fwrite(&score, sizeof(int), 1, hs);
-    fclose(hs);
+    while (fgets(str, MAXCHAR, input_file) != NULL) {
+        fputs(str, output_file);
+    }
+    fclose(output_file);
+}
+
+uint8_t encode(op_code opcode){
+
 }
